53dcc246ff2ba4943816d8ab01020ea967a6122f
Saviour <owolabisaviour@gmail.com>
2022-08-27 16:51:00 +0100

    Adjust to handle complex numbers properly, optimizations

--- verilog_src/FFT_input.v
+++ verilog_src/FFT_input.v
@@ -5,40 +5,55 @@ module fftInput #(
 ) (
     input wire clk, reset, en, 
     input wire in_valid, 
-    input wire [word_size-1: 0] sample1, sample2, 
-    output reg [word_size-1: 0] comp1, comp2,
+    input wire [word_size*2-1: 0] sample1, sample2, 
+    output reg [word_size*2-1: 0] comp1, comp2,
     output reg [address_width-1: 0] addr1, addr2,
     output reg wr_en, done
 );
     
+initial begin
+    $dumpfile("fftInput.vcd");
+    $dumpvars(0, fftInput);
+end
 
 
-
-always @(posedge clk) begin
+always @(posedge clk, posedge reset) begin
 
     if (reset) begin
         comp1 <= 0;
         comp2 <= 0;
         addr1 <= 0;
-        addr2 <= 2;
-        wr_en <= 0;
+        // addr2 <= 1;
+        wr_en <= 0; 
         done <= 0; 
     end
 
-    else begin
-        if (in_valid & en) begin
-            if (addr2 == N-1)
-                done <= 1'b1; 
-            else begin
-                wr_en <= 1'b1; 
-                comp1 <= sample1 + sample2;
-                comp2 <= sample1 - sample2;
-                addr1 <= addr1 + 2;
-                addr2 <= addr2 + 2; 
-            end
+    else if (addr2 == N-1) begin
+        done <= 1;
+        wr_en <= 0;
+    end else begin     
+        comp1 <= {Cr, Ci}; 
+        comp2 <= {Dr, Di}; 
+        addr1 <= addr1 + 2;
+        // addr2 <= addr2 + 2;
+        wr_en <= in_valid & en;
         end
-    end  
+    end        
+
+reg [word_size-1: 0] Cr, Ci, Dr, Di;
+
+always @(*) begin
+    Cr = sample1[word_size*2-1: word_size] + sample2[word_size*2-1: word_size];
+    Ci = sample1[word_size-1: 0] + sample2[word_size-1: 0];
+    Dr = sample1[word_size*2-1: word_size] - sample2[word_size*2-1: word_size];
+    Di = sample1[word_size-1: 0] - sample2[word_size-1: 0];
 end
+
+always @(*) begin
+    // Since addr1 is always even(lsb is 0), addr1 + 1 == addr1 | 1
+    addr2 <= addr1 | 1'b1;
+end
+
 
 
 endmodule
