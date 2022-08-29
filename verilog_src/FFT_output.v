09b1a7fe855b2fb4b8a38006a78c77a5d10713a8
Saviour <owolabisaviour@gmail.com>
2022-08-29 02:11:09 +0100

    Update: Minor optimizations

--- verilog_src/FFT_output.v
+++ verilog_src/FFT_output.v
@@ -4,35 +4,42 @@ module fftOutput #(
     address_width = $clog2(N)
 ) (
     input wire clk, reset, en,
-    input wire [word_size-1: 0] in_samp1, in_samp2,
+    input wire [2*word_size-1: 0] in_samp1, in_samp2,
     output reg [address_width-1: 0] out_addr1, out_addr2,
-    output reg [word_size-1: 0] out_samp1, out_samp2, 
+    output reg [2*word_size-1: 0] out_samp1, out_samp2, 
     output reg out_valid, rd_en, busy
 );      
     
 
-always @(posedge clk) begin
+always @(posedge clk, posedge reset) begin
+    out_samp1 <= in_samp1;
+    out_samp2 <= in_samp2;
+
     if (reset) begin
         out_addr1 <= 0;
-        out_addr2 <= 1;
-        out_valid <= 0;
+        // out_addr2 <= 1;
+        out_valid <= 1'b0;
         rd_en <= 0;
         busy <= 0; 
     end
 
     else if (en) begin
         if (out_addr2 == N-1) begin
-            busy <= 0; 
+            // out_addr1 <= 0; 
+            busy <= 0;
+            out_valid <= 0; 
+            rd_en <= 0; 
         end else begin
             busy <= 1; 
             out_valid <= 1; 
             rd_en <= 1;
-            out_samp1 <= in_samp1;
-            out_samp2 <= in_samp2;
             out_addr1 <= out_addr1 + 2;
-            out_addr2 <= out_addr1 + 2;
+            // out_addr2 <= out_addr1 + 2;
         end 
     end
+end
 
+always @(*) begin
+    out_addr2 = out_addr1 | 1; 
 end
 endmodule
