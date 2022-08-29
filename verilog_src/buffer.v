1c5de748b0ecae8779b7ece7f1f1d0692df2a15e
Saviour <owolabisaviour@gmail.com>
2022-08-29 02:13:59 +0100

    Update: Belated commit -complete buffer

--- verilog_src/buffer.v
+++ verilog_src/buffer.v
@@ -2,7 +2,7 @@ module buffer #(
     parameter vector_size = 16,
     buffer_length = 1
 ) (
-    input wire clk, en, in_valid,  
+    input wire clk, en, in_valid, reset,   
     input wire [vector_size-1: 0] d_in,
     output reg [vector_size-1: 0] d_out,
     output reg d_valid
@@ -13,8 +13,14 @@ module buffer #(
 reg [vector_size: 0] ibuffer [buffer_length-1: 0];
 integer i;
 
-always @(posedge clk) begin
-    if (en) begin
+always @(posedge clk, posedge reset) begin
+
+    if (reset) begin
+        for (i = 0; i <= buffer_length-2; i = i + 1) begin
+            ibuffer[i] <= 0;
+        end
+    end
+    else if (en) begin
         ibuffer[buffer_length-1] <= {in_valid, d_in};
 
         //generate
