c5e02aa4e18a704eb6056d0fcbe9e5c29525ae0e
Saviour <owolabisaviour@gmail.com>
2022-08-29 02:10:31 +0100

    Update: Regularize interface and parameters

--- verilog_src/cRAM.v
+++ verilog_src/cRAM.v
@@ -4,29 +4,38 @@
 // Let's get it !!!
 
 module cRAM #(
-    parameter vector_size = 16,
-    N = 20
+    parameter N = 32,
+    word_size = 16,
+    address_width = $clog2(N)
+    
 ) (
     input wire clk,
-    input wire [$clog2(N)-1:0] read_address, write_address,
-    input wire wr_en, sel, 
-    input wire [vector_size-1: 0] in_real, in_im,
-    output wire [vector_size-1: 0] out_real, out_im
+    input wire [$clog2(N)-1:0] address1, address2,
+    input wire wr_en, sel, read_en,  
+    input wire [word_size*2-1: 0] in1, in2,
+    output reg [word_size*2-1: 0] out1, out2
 );
     
     // RAM DECLARATION -> complex and real concatenated into a single
     //  2*vector_size integer
     
-    reg [2*vector_size-1: 0] memory [N-1:0] ;
-    wire [2*vector_size-1: 0] combo_in;
+    reg [2*word_size-1: 0] memory [N-1: 0];
 
     //assign combo_in = {in_real, in_im}; 
 
-    always @(posedge clk)
-        if (wr_en & sel)
-            memory[write_address] <= {in_real, in_im}; 
+    always @(posedge clk) begin
         
-    assign out_real = memory[read_address][2*vector_size-1:vector_size];
-    assign out_im = memory[read_address][vector_size-1:0];
+        if (wr_en & sel) begin
+            memory[address1] <= in1;
+            memory[address2] <= in2;
+        end
+
+
+        else if (~wr_en & sel)begin
+            out1 <= memory[address1];
+            out2 <= memory[address2];
+        end
+    end
+
 
 endmodule
