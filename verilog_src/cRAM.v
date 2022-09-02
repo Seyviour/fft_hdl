5c345c08a4d2541a45c98698e38b1ea7e0ebad0c
Saviour <owolabisaviour@gmail.com>
2022-09-02 19:00:47 +0100

    Update: add wr_complete and read_en signals

--- verilog_src/cRAM.v
+++ verilog_src/cRAM.v
@@ -13,28 +13,40 @@ module cRAM #(
     input wire [$clog2(N)-1:0] address1, address2,
     input wire wr_en, sel, read_en,  
     input wire [word_size*2-1: 0] in1, in2,
-    output reg [word_size*2-1: 0] out1, out2
+    output reg [word_size*2-1: 0] out1, out2,
+    output reg o_valid, wr_complete
 );
     
+    integer idx;
+    initial begin
+        $dumpfile("cRAM.vcd");
+        $dumpvars(0, cRAM);
+        for (idx = 0; idx < N; idx = idx + 1) begin
+            $dumpvars(0, memory1[idx]);
+        end
+    end
     // RAM DECLARATION -> complex and real concatenated into a single
     //  2*vector_size integer
     
-    reg [2*word_size-1: 0] memory [N-1: 0];
+    reg [2*word_size-1: 0] memory1 [N-1: 0];
 
     //assign combo_in = {in_real, in_im}; 
 
     always @(posedge clk) begin
         
         if (wr_en & sel) begin
-            memory[address1] <= in1;
-            memory[address2] <= in2;
+            memory1[address1] <= in1;
+            memory1[address2] <= in2;
         end
 
 
-        else if (~wr_en & sel)begin
-            out1 <= memory[address1];
-            out2 <= memory[address2];
+        else if (read_en & sel)begin
+            out1 <= memory1[address1];
+            out2 <= memory1[address2];
         end
+
+        wr_complete <= wr_en;
+        o_valid <= read_en; 
     end
 
 
