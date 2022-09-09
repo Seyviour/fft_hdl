5e20a31996573100d98768c96d6f0bfa1bc5d33c
Saviour <owolabisaviour@gmail.com>
2022-09-09 19:53:48 +0100

    Do bit reversal instead of first level computation

--- verilog_src/FFT_input.v
+++ verilog_src/FFT_input.v
@@ -7,7 +7,7 @@ module fftInput #(
     input wire in_valid, 
     input wire [word_size*2-1: 0] sample1, sample2, 
     output reg [word_size*2-1: 0] comp1, comp2,
-    output reg [address_width-1: 0] addr1, addr2,
+    output wire [address_width-1: 0] addr1, addr2,
     output reg wr_en, busy, o_input_valid
 );
     
@@ -21,14 +21,18 @@ wire start;
 assign start = en && in_valid;
 
 
+reg [address_width-1: 0] addr1_i, addr2_i;
+
 always @(posedge clk, posedge reset) begin
 
-    comp1 <= {Cr, Ci}; 
-    comp2 <= {Dr, Di}; 
+    // comp1 <= {Cr, Ci}; 
+    // comp2 <= {Dr, Di}; 
                 
+    comp1 <= sample1; 
+    comp2 <= sample2; 
+
     if (reset) begin
-        addr1 <= 0;
-        // addr2 <= 1;
+        addr1_i <= 0;
         wr_en <= 0; 
         busy <= 0;
         o_input_valid <= 0; 
@@ -39,31 +43,39 @@ always @(posedge clk, posedge reset) begin
             o_input_valid <= 1'b0; 
             busy <= 1'b1;
             wr_en <= 1'b1; 
-            if (addr2 == N-1) begin
+            if (addr2_i == N-1) begin
                 busy <= 1'b0;
                 wr_en <= 1'b0;
                 o_input_valid <= 1'b1; 
             end else begin 
                 if (busy)
-                    addr1 <= addr1 + 2;
-                
+                    addr1_i <= addr1_i + 2;
             end
         end
     end        
 end 
-reg [word_size-1: 0] Cr, Ci, Dr, Di;
+// reg [word_size-1: 0] Cr, Ci, Dr, Di;
 
-always @(*) begin
-    Cr = sample1[word_size*2-1: word_size] + sample2[word_size*2-1: word_size];
-    Ci = sample1[word_size-1: 0] + sample2[word_size-1: 0];
-    Dr = sample1[word_size*2-1: word_size] - sample2[word_size*2-1: word_size];
-    Di = sample1[word_size-1: 0] - sample2[word_size-1: 0];
-end
+// always @(*) begin
+//     Cr = sample1[word_size*2-1: word_size] + sample2[word_size*2-1: word_size];
+//     Ci = sample1[word_size-1: 0] + sample2[word_size-1: 0];
+//     Dr = sample1[word_size*2-1: word_size] - sample2[word_size*2-1: word_size];
+//     Di = sample1[word_size-1: 0] - sample2[word_size-1: 0];
+// end
 
 always @(*) begin
     // Since addr1 is always even(lsb is 0), addr1 + 1 == addr1 | 1
-    addr2 <= addr1 | 1'b1;
+    addr2_i = addr1_i | 1'b1;
 end
+
+genvar i;
+generate
+    for (i=0; i<address_width; i=i+1) begin
+        assign addr1[i] = addr1_i[address_width-1-i];
+        assign addr2[i] = addr2_i[address_width-1-i];
+    end
+endgenerate
+
 
 
 
